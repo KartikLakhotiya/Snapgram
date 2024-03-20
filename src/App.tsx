2910b080f96ec531fe00114e4c2f044398bb0408
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-03-21 00:59:22 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './globals.css';
+import {Routes, Route} from 'react-router-dom';
+import  SigninForm  from './_auth/forms/SigninForm';
+import  Home  from './_root/pages/Home';
+import  SignupForm  from './_auth/forms/SignupForm';
+import  AuthLayout  from './_auth/AuthLayout';
+import  RootLayout  from './_root/RootLayout';
+
+export const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout/>}>
+                <Route path='/sign-in' element={<SigninForm/>}></Route>
+                <Route path='/sign-up' element={<SignupForm/>}></Route>
+            </Route>
+
+            {/* private routes */}
+            <Route element={<RootLayout/>}>
+                <Route index element={<Home/>}></Route>
+            </Route>
+
+        </Routes>
+    </main>
+  )
+}
